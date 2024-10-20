9e37158af065adf9025a02993af7333ee64cbc41
luljn <mbohlulajonathan4@gmail.com>
2024-10-20 23:59:32 +0200

    UI refactored

--- model/factory.py
+++ model/factory.py
@@ -13,9 +13,9 @@ class Factory :
     
     def buttonFactory(self, window) : 
         
-        start_game_button = Button(pygame.image.load("resources\img\Rect.png"), (980, 300), "Jouer", self.font.getFont(25), "White", "Blue")
-        options_button = Button(pygame.image.load("resources\img\Rect.png"), (980, 420), "Options", self.font.getFont(25), "White", "Blue")
-        quit_button = Button(pygame.image.load("resources\img\Rect.png"), (980, 540), "Quitter", self.font.getFont(25), "White", "Blue")
+        start_game_button = Button(pygame.image.load("resources\img\Rect.png"), (window.getScreenWidth() / 1.27, window.getScreenHeight() / 4), "Jouer", self.font.getFont(25), "White", "Blue")
+        options_button = Button(pygame.image.load("resources\img\Rect.png"), (window.getScreenWidth() / 1.27, 420), "Options", self.font.getFont(25), "White", "Blue")
+        quit_button = Button(pygame.image.load("resources\img\Rect.png"), (window.getScreenWidth() / 1.27, 540), "Quitter", self.font.getFont(25), "White", "Blue")
         
         buttons = [start_game_button, options_button, quit_button]
         
