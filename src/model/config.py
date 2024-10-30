9e55bc2ab673d4f78728fb1e2ee11e3bb655f2db
luljn <mbohlulajonathan4@gmail.com>
2024-10-30 20:53:04 +0100

    Config management class created

---
+++ src/model/config.py
@@ -0,0 +1,15 @@
+#Configurations management class.
+
+
+
+class Config : 
+    
+    @staticmethod
+    def loadConfig() :
+        
+        pass
+    
+    @staticmethod
+    def saveConfigs() :
+        
+        pass
