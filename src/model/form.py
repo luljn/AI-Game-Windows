748b4f5875f1a1dbbfd5f13842677cb23f12025d
luljn <mbohlulajonathan4@gmail.com>
2024-11-04 22:37:37 +0100

    Code refactored 5.1

--- src/model/form.py
+++ src/model/form.py
@@ -14,8 +14,8 @@ class Form :
     
     def move(self, dt) :
         
-        # self.drawSprite()
         self.keys = pygame.key.get_pressed()
+        
         if self.keys[pygame.K_UP] : 
             
             self.position.y -= 300 * dt
@@ -23,15 +23,15 @@ class Form :
         if self.keys[pygame.K_DOWN] : 
             
             self.position.y += 300 * dt
-            
+        
         if self.keys[pygame.K_LEFT] : 
             
             self.position.x -= 300 * dt
-            
+        
         if self.keys[pygame.K_RIGHT] : 
             
             self.position.x += 300 * dt
-            
+        
         dt = self.window.clock.tick(60) / 1000
     
     def drawSprite(self) :
