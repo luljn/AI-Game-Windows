62dec60c4c00909cb5218fffbb239d16371ba971
luljn <mbohlulajonathan4@gmail.com>
2024-11-09 19:25:52 +0100

    Class Square upgraded

--- src/model/square.py
+++ src/model/square.py
@@ -8,20 +8,31 @@ class Square(Form) :
     #Empty square (the central at the beginning of the game).
     empty_square_id = 4
     
-    def __init__(self, id, window, width, height, position, color = "Blue") :
+    def __init__(self, id, window, width, height, position, color = "Blue", image = None) :
         
         super().__init__(window, position)
         self.id = id
         self.width = width
         self.height = height
         self.color = color
+        self.image = image
         # self.width = self.window.getScreenWidth() / 16
         # self.height = self.window.getScreenHeight() / 10
         # self.position = pygame.Vector2((self.window.getScreenWidth() / 2) -  self.width / 2 , (self.window.getScreenHeight() / 2) - self.height /2)
+        
+        self.rect = self.image.get_rect(center = (self.position_x, self.position_y))
     
     def drawSprite(self) :
         
         pygame.draw.rect(self.window.screen, self.color, (self.position.x, self.position.y, self.width, self.height))
+    
+    def checkPosition(self, position) :
+        
+        if (position[0] in range (self.rect.left, self.rect.right) and position[1] in range (self.rect.top, self.rect.bottom)):
+            
+            return True
+        
+        return False
     
     def getId(self) :
         
