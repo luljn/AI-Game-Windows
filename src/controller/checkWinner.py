2dc742d4080bd2571e6f2e6780660ffa1c50f5bc
luljn <mbohlulajonathan4@gmail.com>
2024-11-28 13:10:52 +0100

    CheckWinner class added

---
+++ src/controller/checkWinner.py
@@ -0,0 +1,13 @@
+# check winner controller.
+
+class CheckWinner :
+    
+    @staticmethod
+    def checkPlayerVsAiWinner(player_pawns, cpu_pawns) :
+        
+        pass
+    
+    @staticmethod
+    def checkAiVsAiWinner(cpu_1_pawns, cpu_2_pawns) :
+        
+        pass
