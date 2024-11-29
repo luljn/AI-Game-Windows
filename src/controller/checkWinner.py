c9a23a0ed980ca76f2cc1f5826c362b2cb721692
luljn <mbohlulajonathan4@gmail.com>
2024-11-29 23:52:52 +0100

    CheckWinner v1.3

--- src/controller/checkWinner.py
+++ src/controller/checkWinner.py
@@ -82,4 +82,76 @@ class CheckWinner :
     @staticmethod
     def checkAiVsAiWinner(cpu_1_pawns, cpu_2_pawns) :
         
-        pass
+        if len(cpu_1_pawns) == 3 and len(cpu_2_pawns) == 3 :
+            
+            # cpu_1
+            # Horizontally
+            if cpu_1_pawns[0].square.id == 0 and cpu_1_pawns[1].square.id == 1 and cpu_1_pawns[2].square.id == 2 :
+                
+                return "cpu_1"
+            
+            elif cpu_1_pawns[0].square.id == 3 and cpu_1_pawns[1].square.id == 4 and cpu_1_pawns[2].square.id == 5 :
+                
+                return "cpu_1"
+            
+            elif cpu_1_pawns[0].square.id == 6 and cpu_1_pawns[1].square.id == 7 and cpu_1_pawns[2].square.id == 8 :
+                
+                return "cpu_1"
+            
+            # Vertically
+            elif cpu_1_pawns[0].square.id == 0 and cpu_1_pawns[1].square.id == 3 and cpu_1_pawns[2].square.id == 6 :
+                
+                return "cpu_1"
+            
+            elif cpu_1_pawns[0].square.id == 1 and cpu_1_pawns[1].square.id == 4 and cpu_1_pawns[2].square.id == 7 :
+                
+                return "cpu_1"
+            
+            elif cpu_1_pawns[0].square.id == 2 and cpu_1_pawns[1].square.id == 5 and cpu_1_pawns[2].square.id == 8 :
+                
+                return "cpu_1"
+            
+            # Diagonally
+            elif cpu_1_pawns[0].square.id == 0 and cpu_1_pawns[1].square.id == 4 and cpu_1_pawns[2].square.id == 8 :
+                
+                return "cpu_1"
+            
+            elif cpu_1_pawns[0].square.id == 2 and cpu_1_pawns[1].square.id == 4 and cpu_1_pawns[2].square.id == 6 :
+                
+                return "cpu_1"
+            
+            # cpu_2
+            # Horizontally
+            if cpu_2_pawns[0].square.id == 0 and cpu_2_pawns[1].square.id == 1 and cpu_2_pawns[2].square.id == 2 :
+                
+                return "cpu_2"
+            
+            elif cpu_2_pawns[0].square.id == 3 and cpu_2_pawns[1].square.id == 4 and cpu_2_pawns[2].square.id == 5 :
+                
+                return "cpu_2"
+            
+            elif cpu_2_pawns[0].square.id == 6 and cpu_2_pawns[1].square.id == 7 and cpu_2_pawns[2].square.id == 8 :
+                
+                return "cpu_2"
+            
+            # Vertically
+            elif cpu_2_pawns[0].square.id == 0 and cpu_2_pawns[1].square.id == 3 and cpu_2_pawns[2].square.id == 6 :
+                
+                return "cpu_2"
+            
+            elif cpu_2_pawns[0].square.id == 1 and cpu_2_pawns[1].square.id == 4 and cpu_2_pawns[2].square.id == 7 :
+                
+                return "cpu_2"
+            
+            elif cpu_2_pawns[0].square.id == 2 and cpu_2_pawns[1].square.id == 5 and cpu_2_pawns[2].square.id == 8 :
+                
+                return "cpu_2"
+            
+            # Diagonally
+            elif cpu_2_pawns[0].square.id == 0 and cpu_2_pawns[1].square.id == 4 and cpu_2_pawns[2].square.id == 8 :
+                
+                return "cpu_2"
+            
+            elif cpu_2_pawns[0].square.id == 2 and cpu_2_pawns[1].square.id == 4 and cpu_2_pawns[2].square.id == 6 :
+                
+                return "cpu_2"
